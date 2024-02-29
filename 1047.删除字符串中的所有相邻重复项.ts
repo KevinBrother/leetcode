d37b3e2a48846f3a98289740a382fe5934eb15c6
caojunjie <1301239018@qq.com>
2024-02-29 15:13:52 +0800

    feat(easy stack) 1047 done

---
+++ 1047.删除字符串中的所有相邻重复项.ts
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=1047 lang=typescript
+ *
+ * [1047] 删除字符串中的所有相邻重复项
+ */
+
+// @lc code=start
+function removeDuplicates(s: string): string {
+    const resultStack: string[] = [];
+    for(let i  = 0; i < s.length; i++) {
+        const char = s[i];
+        if(resultStack.length && resultStack[resultStack.length - 1] === char) {
+            resultStack.pop();
+        }else {
+            resultStack.push(char);
+        }
+    }
+    return resultStack.join('')
+};
+
+console.log('remove', removeDuplicates('abbaca'))
+// @lc code=end
+
