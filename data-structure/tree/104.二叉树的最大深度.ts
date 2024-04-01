196b1a32223b67f3c3fc150d15b11f7d21cb6586
caojunjie <1301239018@qq.com>
2024-04-01 10:29:34 +0800

    feat(tree) 104 树最大深度的return版本

---
+++ data-structure/tree/104.二叉树的最大深度.ts
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=typescript
+ *
+ * [104] 二叉树的最大深度
+ */
+
+import { TreeNode } from "../../util/listNode";
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+// function recursive(node:TreeNode | null) {
+//     if(node === null) {
+//         return null;
+//     }
+//     recursive(node.left);
+//     recursive(node.right);
+// }
+
+function recursive(node: TreeNode | null) {
+  if (node === null) {
+    return null;
+  }
+  recursive(node.left);
+  recursive(node.right);
+}
+
+function maxDepth1(root: TreeNode | null): number {
+  let result = 0;
+
+  let max = 0;
+  function recursive1(node: TreeNode | null) {
+    if (node === null) {
+      result = Math.max(result, max);
+      return;
+    }
+    ++max;
+    recursive(node.left);
+    recursive(node.right);
+    --max;
+  }
+
+  recursive(root);
+
+  return result;
+}
+
+
+
+function maxDepth(root: TreeNode | null): number {
+  function recursive(node: TreeNode | null) {
+    if (node === null) {
+      return 0;
+    }
+   
+    return Math.max(recursive(node.left), recursive(node.right)) + 1
+  }
+
+  return recursive(root);
+}
+
+// @lc code=end
