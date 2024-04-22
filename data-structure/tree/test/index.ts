94d01a1519e68a173df3a7a26ad19d7ccfacd02b
caojunjie <1301239018@qq.com>
2024-04-22 10:59:47 +0800

    test(bfs)

--- data-structure/tree/test/index.ts
+++ data-structure/tree/test/index.ts
@@ -9,14 +9,26 @@
 //         / \
 //        15  7 
 
+
+
+// console.log('first', constructFromPrePost([1,2,4,5,3,6,7], [4,2,5,1,6,3,7]))
+//       1 
+//      /  \
+//    2     3
+//   / \    / \
+//  4   5  6   7 
+
 import { levelOrder } from "../05.bfs/102.二叉树的层序遍历";
 import { bfs } from "../05.bfs/bfs";
 import { buildTree } from "../105.从前序与中序遍历序列构造二叉树";
 
-let root = buildTree([3,9,20,15,7],  [9,3,15,20,7]);
+let root1 = buildTree([3,9,20,15,7],  [9,3,15,20,7]);
+let root2 = buildTree([1,2,4,5,3,6,7], [4,2,5,1,6,3,7])
 
-// console.log('bfs(root)', bfs(root));
+// console.log('bfs(root)', bfs(root1));
+console.log('levelOrder(root)', levelOrder(root2));
 
-console.log('levelOrder(root)', levelOrder(root))
+
+
 
 
