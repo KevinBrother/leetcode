d30bab96cf27111311334087a01854ceda8ea386
caojunjie <1301239018@qq.com>
2024-06-11 09:52:22 +0800

    chore(78): formate

--- algorithms/backtracking/01.子集型/78.子集.ts
+++ algorithms/backtracking/01.子集型/78.子集.ts
@@ -6,35 +6,59 @@
 
 // @lc code=start
 function subsets(nums: number[]): number[][] {
+  let rst: number[][] = [];
+  let path: number[] = [];
+  let len = nums.length;
 
-    let rst: number[][] = [];
-    let path: number[] = [];
-    let len = nums.length;
-
-    function dfs(i) {
-        if(i === len) {
-            rst.push([...path]);
-            return;
-        }
-
-        // 不选, 直接递归
-        dfs(i + 1);
-
-        // 选，先操作，在递归
-        path.push(nums[i])
-        dfs(i+1)
-
-        // 恢复现场，递归之前是什么样，递归之后也应该是什么样的
-        path.pop();
-
+  function dfs(i) {
+    if (i === len) {
+      rst.push([...path]);
+      return;
     }
 
-    dfs(0);
+    // 不选, 直接递归
+    dfs(i + 1);
 
-    return rst;
-};
+    // 选，先操作，在递归
+    path.push(nums[i]);
+    dfs(i + 1);
 
-console.log("🚀 ~ subsets ~ subsets:", subsets([1,2]))
+    // 恢复现场，递归之前是什么样，递归之后也应该是什么样的
+    path.pop();
+  }
+
+  dfs(0);
+
+  return rst;
+}
+
+// console.log("🚀 ~ subsets ~ subsets:", subsets([1,2]))
 
 // @lc code=end
+
+// 输出的角度，每次必须选一个数
+function subsets2(nums: number[]): number[][] {
+  let rst: number[][] = [];
+
+  let path: number[] = [];
+
+  function dfs(i) {
+    rst.push(Array.from(path))
+    if (i === nums.length) {
+      return;
+    }
+
+    for (let j = i; j < nums.length; j++) {
+      path.push(nums[j]);
+      dfs(i + 1);
+      path.pop();
+    }
+  }
+
+  dfs(0);
+
+  return rst;
+}
+
+console.log("🚀 ~ subsets ~ subsets:", subsets2([1,2]))
 
