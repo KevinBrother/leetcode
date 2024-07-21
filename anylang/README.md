d650187091259024388896e6e19693a3c337357e
caojunjie <1301239018@qq.com>
2024-07-22 01:00:41 +0800

    feat(lang-sum): 各个语言的数据结构总结

--- anylang/README.md
+++ anylang/README.md
@@ -9,6 +9,39 @@
 [编程入门](https://leetcode.cn/studyplan/primers-list/)
 [编程基础 0 到 1](https://leetcode.cn/studyplan/programming-skills/)
 
+### 数据结构总结
+
+#### 字符串
+
+``` go
+// 定义
+var str1 = "str1"
+var str2 string = "str2"
+str3 := "str3"
+
+// 获取长度
+len(str1);
+
+
+// 获取第一个字符
+str1[0]
+
+// 修改第一个字符
+
+```
+
+#### 数组
+
+``` go
+
+
+```
+
+#### map
+
+``` go
+```
+
 ## 设计模式
 
 ...
--- anylang/go/test/test.go
+++ anylang/go/test/test.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func add(x, y int) (z int) {
 	defer func() {
 		println(z) // 输出: 203
@@ -10,5 +12,16 @@ func add(x, y int) (z int) {
 }
 
 func main() {
-	println(add(1, 2)) // 输出: 203
+	// println(add(1, 2)) // 输出: 203
+	dataStruct()
+}
+
+func dataStruct() {
+	// string
+	var str1 = "str1"
+	var str2 string = "str2"
+	str3 := "str3"
+
+	fmt.Println(str1, str2, str3, len(str1), str1[0])
+
 }
